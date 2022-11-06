1f6c037978a51e7c0d2e77d1d862464f1a318af6
dimitrii-ben <dimitrii.benoit@hotmail.com>
2022-11-06 16:17:19 +0000

    Add limit

--- TP2/src/main.c
+++ TP2/src/main.c
@@ -1,28 +1,42 @@
 #include "langton.h"
 /* argv[]
- * 0: Frame Per Second
+ * 0: temps d'attendte entre chaque frame en seconde
  * 1: Nb d'iterations
 */
 int main(int argc,char* argv[]){
-    ETAT* head = createFirstState(NORD,25,25);
-    ETAT* current_pt = head;
-    for(int i =0;i<10000;i++){
-        createNextState(head);
+    if(argc!=3){
+        printf("Exit failure\n");
+        return EXIT_FAILURE;
     }
-    for (int i =0;i<10000;i++){
+    ETAT* head = createFirstState(NORD,25,25);
+    ETAT* current_pt;
+    int iteration = 0;
+    int limite = 0;
+    while(!limite && iteration <atoi(argv[2])){
+        current_pt = createNextState(head);
+        if(current_pt->x_fourmi > DIMX || current_pt->x_fourmi <0 || current_pt->y_fourmi <0 || current_pt->y_fourmi>DIMY){
+            limite =1;
+            printf("LIMITE : %i",iteration);
+            
+        }
+        else{
+            iteration++;
+        }
+        
+
+    }
+    
+    
+    current_pt = head;
+    for (int i =0;i<=iteration;i++){
         displayState(current_pt);
         current_pt = current_pt->next;
-        printf("\e[1;1H\e[2J");
+        printf("\e[1;1H\e[2J");        
+        usleep(atoi(argv[1])); 
+        
+        
 
     }
     freeAll(head);
-    /*for (int i =0;i<atoi(argv[1]);i++){
-        createNextState(head);
-    }
-    ETAT* current_pt= head;
-    for (int i =0;i<atoi(argv[1]);i++){
-
-        displayState(current_pt);
-        current_pt = current_pt->next;
-    }*/
+    return EXIT_SUCCESS;
 }
